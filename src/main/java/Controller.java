a9855c60fdd6e46c5d9dcceafc1c418ed7b2e3c5
Igor <78314860+illa911@users.noreply.github.com>
2021-03-05 19:11:46 +0500

    Initial commit

---
+++ src/main/java/Controller.java
@@ -0,0 +1,16 @@
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+    @org.springframework.stereotype.Controller
+
+    public class Controller {
+
+        @RequestMapping(value = "/greeting")
+        public String helloWorldController(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
+            model.addAttribute("name", name);
+            return "greeting";
+        }
+    }
+
