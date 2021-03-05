81ffcf9142cc0a155c1d7eb55afdff71f9296017
Igor <78314860+illa911@users.noreply.github.com>
2021-03-05 19:13:07 +0500

    Lesson1

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -3,14 +3,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-    @org.springframework.stereotype.Controller
+@org.springframework.stereotype.Controller
 
-    public class Controller {
+public class Controller {
 
-        @RequestMapping(value = "/greeting")
-        public String helloWorldController(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
-            model.addAttribute("name", name);
-            return "greeting";
-        }
+    @RequestMapping(value = "/greeting")
+    public String helloWorldController(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
     }
+}
 
